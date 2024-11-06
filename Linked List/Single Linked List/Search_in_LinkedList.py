c00ff446a92e2f246004b5782fec09a0ebd5076c
NamanSaxena3108 <naman2004saxena@gmail.com>
2024-11-06 16:02:31 +0530

    Updated LinkedList

---
+++ Linked List/Single Linked List/Search_in_LinkedList.py
@@ -0,0 +1,85 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        temp_node = self.head
+        result = ' '
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result +=  ' -> '
+            temp_node = temp_node.next
+        return result
+    
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def Prepend(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def insert(self,index,value):
+        new_node = Node(value)
+        if index < 0 or index > self.length:  #Checking for range of link list
+            return False
+        if self.length == 0:    # if there is no link list
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:         # if we are inserting at start
+            new_node.next = self.head
+            self.head = new_node
+        else:                    # normal insertion at given index
+            temp_node = self.head
+            for _ in range(index-1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+        self.length += 1
+        return True
+    
+    def traverse(self):
+        current = self.head
+        while current is not None:
+            print(current.value)
+            current = current.next
+
+    def search(self, target):
+        current = self.head
+        index = 0
+        while current is not None:
+            if current.value == target:
+                return index
+            current = current.next
+            index += 1
+        return -1
+
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+print(new_linked_list)
+print(new_linked_list.search(30))
+
+#Time Complexity = O(n)
+#Space Complexity = O(1)
